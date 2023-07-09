57c6c9c0f50cfcc57b8cfcf4e6a64c9080da4ccd
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-10 00:27:08 +0530

    [Amit]Add : Prime Number

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/PrimeNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class PrimeNumber
+    {
+        public void Prime()
+        {
+            Console.WriteLine("Enter Starting Number : ");
+            int start = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Enter Ending Number : ");
+            int end = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Prime numbers between " + start + " and " + end + " are : ");
+            int count;
+            for (int i = start; i <= end; i++)
+            {
+                count = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                        count++;
+                }
+                if (count == 2)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
--- DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("1. Insertion Sort");
+            Console.WriteLine("2. Prime Number");
             int choice = Convert.ToInt32(Console.ReadLine());
 
             switch (choice)
@@ -14,6 +15,10 @@
                     Insertion_Sort insertion_Sort = new Insertion_Sort();
                     insertion_Sort.Insertion(arr);
                     insertion_Sort.Display(arr);
+                    break;
+                case 2:
+                    PrimeNumber primeNumber = new PrimeNumber();
+                    primeNumber.Prime();
                     break;
                 default:
                     Console.WriteLine("Enter avslid choice");
