da62aaf07f6ad5e0fe413690e5071df9c2309d9a
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-10 00:18:04 +0530

    [Amit]Add : InsertionSort

---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Insertion_Sort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresAndAlgorithms
+{
+    public class Insertion_Sort
+    {
+        public void Insertion(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > temp)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = temp;
+            }
+        }
+        public void Display(int[] intArr)
+        {
+            for (int index = 0; index < intArr.Length; index++)
+            {
+                Console.WriteLine(intArr[index]);
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/DataStructuresAndAlgorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DataStructuresAndAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Insertion Sort");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    int[] arr = { 1, 65, 65, 4, 54, 88, 54 };
+                    Insertion_Sort insertion_Sort = new Insertion_Sort();
+                    insertion_Sort.Insertion(arr);
+                    insertion_Sort.Display(arr);
+                    break;
+                default:
+                    Console.WriteLine("Enter avslid choice");
+                    break;
+            }
+        }
+    }
+}
